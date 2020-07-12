5846979cde5672deebf7a081b5a0dadf16487d50
Shafiul <dshafiul@netmediamobile.com>
2020-07-12 18:31:06 +0600

    Sw js updated.

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('This is a test message from push', {});
-});
-
+function receivePushNotification(event) {
+    console.log("[Service Worker] Push Received.");
+  
+    const { image, tag, url, title, text } = event.data.json();
+  
+    const options = {
+      data: url,
+      body: text,
+      icon: image,
+      vibrate: [200, 100, 200],
+      tag: tag,
+      image: image,
+      badge: "https://spyna.it/icons/favicon.ico",
+      actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+  }
