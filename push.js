03cff169f76ba7401c1a93e2fc0f6847e1371b93
Shafiul <dshafiul@netmediamobile.com>
2020-07-12 18:55:00 +0600

    Files added for remove errors

--- push.js
+++ push.js
@@ -7,8 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mail:sbashar04@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = { endpoint: "https://fcm.googleapis.com/fcm/send/dYTHY97UeZY:APA91bHxFCRAW_f9lzlzVo4HZZXeEdxs_2d0XPaWF2SEamUsvmR7RQHQIMnlfCotcRTn6leo5s6k5aj_PxLqNd9xiku9Kxd_bUqkMLyKDA-lKRkhbNfARsPKndFeyKPkbBaXltpGh2bK", 
-expirationTime: null, 
-keys: { p256dh: "BAEKn0Buxo4ipWalg1FMDkFuYO8ezgXe1XEz-a5BzHvH_vQ4e7BxZmIj3_xGMUPufU-Uo2VzSo1oalYosfyRm3Q", auth: "YGk-Pqm8iSiSFtI970Ie1A" } };
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dYTHY97UeZY:APA91bHxFCRAW_f9lzlzVo4HZZXeEdxs_2d0XPaWF2SEamUsvmR7RQHQIMnlfCotcRTn6leo5s6k5aj_PxLqNd9xiku9Kxd_bUqkMLyKDA-lKRkhbNfARsPKndFeyKPkbBaXltpGh2bK","expirationTime":null,"keys":{"p256dh":"BAEKn0Buxo4ipWalg1FMDkFuYO8ezgXe1XEz-a5BzHvH_vQ4e7BxZmIj3_xGMUPufU-Uo2VzSo1oalYosfyRm3Q","auth":"YGk-Pqm8iSiSFtI970Ie1A"}};
 
 push.sendNotification(sub, 'New Message from Shafiul Bashar');
