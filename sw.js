13cb8ebed6b660ea2cbe2c0717ccd964c2de5ef1
Shafiul <dshafiul@netmediamobile.com>
2020-07-15 21:23:40 +0600

    Notification click event added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('push', function(event) {
+self.addEventListener('push', function (event) {
     var pData = event.data.json(); // Get the data pushed from server.
     console.log(pData);
     const options = {
@@ -6,23 +6,28 @@ self.addEventListener('push', function(event) {
         icon: pData.data.icon,
         vibrate: [100, 50, 100],
         tag: 'push_notify',
-        // data: {
-        //     dateOfArrival: Date.now(),
-        //     primaryKey: '2'
-        // },
+        image: pData.data.image,
+        lang: pData.data.lang,
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1,
+            seeWebLink: pData.data.web_link
+        },
         actions: [
             { action: "see_web", title: "See in Web", icon: pData.data.positive_icon },
             { action: "close", title: "Close", icon: pData.data.negative_icon }
-        ],
-        onClick: function(){
-            window.open("https://www.google.com");
-        }
+        ]
     };
     event.waitUntil(self.registration.showNotification(pData.data.title, options));
 })
 
-self.addEventListener('notificationclick', function(event) {    
-        event.notification.close();  
-        console.log(event);
-    }, false
-);
+self.addEventListener('notificationclick', function (event) {
+    var notification = event.notification;
+    var action = event.action;
+    if (action == 'see_web') {
+        clients.openWindow(notification.data.seeWebLink);
+        notification.close();
+    } else {
+        notification.close();
+    }
+}, false);
