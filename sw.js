a6cbc8575aed54f26992b919f25ffb99c6142a0b
Shafiul <dshafiul@netmediamobile.com>
2020-07-15 21:02:57 +0600

    Onclick event

--- sw.js
+++ sw.js
@@ -11,9 +11,18 @@ self.addEventListener('push', function(event) {
         //     primaryKey: '2'
         // },
         actions: [
-            { action: "explore", title: "Explore this new world", icon: pData.data.positive_icon },
+            { action: "see_web", title: "See in Web", icon: pData.data.positive_icon },
             { action: "close", title: "Close", icon: pData.data.negative_icon }
-        ]
+        ],
+        onClick: function(){
+            window.open("https://www.google.com");
+        }
     };
     event.waitUntil(self.registration.showNotification(pData.data.title, options));
 })
+
+self.addEventListener('notificationclick', function(event) {    
+        event.notification.close();  
+        console.log(event);
+    }, false
+);
