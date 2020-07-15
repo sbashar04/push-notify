d813331501702e8ff6c359d9e0a41c6444814a1e
Shafiul <dshafiul@netmediamobile.com>
2020-07-15 20:21:08 +0600

    sw file updated.

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 self.addEventListener('push', function(event) {
-    var pData = event.data.json();
+    var pData = event.data.json(); // Get the data pushed from server.
     console.log(pData);
     const options = {
-        body: pData.body,
-        icon: pData.icon,
+        body: pData.data.body,
+        icon: pData.data.icon,
         vibrate: [100, 50, 100],
         tag: 'push_notify',
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: '2'
-        },
+        // data: {
+        //     dateOfArrival: Date.now(),
+        //     primaryKey: '2'
+        // },
         actions: [
-            { action: "explore", title: "Explore this new world", icon: pData.positive_icon },
-            { action: "close", title: "Close", icon: pData.negative_icon }
+            { action: "explore", title: "Explore this new world", icon: pData.data.positive_icon },
+            { action: "close", title: "Close", icon: pData.data.negative_icon }
         ]
     };
-    event.waitUntil(self.registration.showNotification(pData.title, options));
+    event.waitUntil(self.registration.showNotification(pData.data.title, options));
 })
